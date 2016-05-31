44379ce20684e6519e0eec4ee0b7bc2d8d5f1d47
UntitledLonelinessProjectOfficial <mac@Macs-MacBook-Pro.local>
2016-05-31 10:21:13 +0800

    Added bullets
    Bullets now move and start at jodie gun, still need to update wible

--- gamedev.js
+++ gamedev.js
@@ -1,11 +1,61 @@
 $( document ).ready(function() {
 
+$( "#jodie" ).hide();
+
 $("#next").click(function(){
 	$( "#talkbox" ).hide();
 	$( "#wible" ).hide();
 	$( "#exploreBody" ).css("backgroundImage","url(img/back3.png)");
+	$("#jodie").show();
+    $('#bulletleft').hide();
+    $('#bulletright').hide();
+
+
+
+
+
+$(document).keydown(function(e) {
+    switch (e.which) {
+    case 37:
+        $("#jodie").stop().animate({
+            left: '-=75'
+        }); //left arrow key
+        break;
+   
+    case 39:
+        $("#jodie").stop().animate({
+            left: '+=75'
+        }); //right arrow key
+        break;
+
+         case 32:
+        $('#bulletright').show();
+        $('#bulletleft').show();
+        $('#bulletleft').animate({
+            left: '-=1000'
+        });
+        $('#bulletright').animate({
+            right: '-=1000'
+        });
+        break;
+    
+    }
+})
+
 
 
 });
+
+
+
+
+
+
 });
+
+
+
+
+
+
 
